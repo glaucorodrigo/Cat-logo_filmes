f064086cc763413bb842712d2e65a609ae0a1eba
Glauco <glauco.rodrigo1@gmail.com>
2022-10-20 17:15:15 -0300

    configuração dos destaques

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './FeaturedMovie.css';
+import tmdb from "../tmdb";
 
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            FILME EM DESTAQUE
-        </div>
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+
+                    
+                </div>
+            </div>
+        </section>
     );
 }
