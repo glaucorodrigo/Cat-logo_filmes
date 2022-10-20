ebc363970309f0770890d01daae046bcfed8de0f
Glauco <glauco.rodrigo1@gmail.com>
2022-10-20 19:20:59 -0300

    Definindo header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+export default () => {
+    return (
+        <header>
+            <div classname="header--logo">
+                <a href="/" 
+                    <img src="./MOVIEFLIX/imagens/logo.png"></img>
+                </a>
+            </div>
+        </header>
+    );
+}
