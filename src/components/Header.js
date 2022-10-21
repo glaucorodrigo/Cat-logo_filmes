dc7c31a8599afccfe660120f5d8f67f3508787fc
Glauco <glauco.rodrigo1@gmail.com>
2022-10-21 00:32:25 -0300

    fixando header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ export default () => {
     return (
         <header>
             <div classname="header--logo">
-                <a href="/" 
-                    <img src="./MOVIEFLIX/imagens/logo.png"></img>
+                <a href="/" >
+                    <img></img>
                 </a>
             </div>
         </header>
-    );
+    )
 }
