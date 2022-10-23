244c24590386fb7bdee48d47a5954daf9c88ebe8
Glauco <glauco.rodrigo1@gmail.com>
2022-10-22 23:46:15 -0300

    ajustes básicos do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './Header.css';
 
+
 export default () => {
     return (
         <header>
             <div classname="header--logo">
                 <a href="/" >
-                    <img></img>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="logo" />
                 </a>
             </div>
         </header>
-    )
+    );
 }
